a62cc390a5a66123477b6346df0aa549362508e2
Pandora <nicolas.jimenez2001@alumnos.ubiobio.cl>
2024-10-20 13:03:11 -0300

    Codigo con mensaje de termino de carrera fixeado

--- trabajo1.cpp
+++ trabajo1.cpp
@@ -6,7 +6,7 @@
 #include <random>
 #include <mutex>
 
-std::mutex mtx; // Mutex para proteger la salida a la consola y el orden de llegada
+std::mutex mtx; 
 
 struct Auto {
     int id;
@@ -19,33 +19,40 @@ struct Auto {
 void correr(Auto &auto_obj, int &posicion_llegada, std::vector<std::pair<int, int>> &resultado) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> avance_dist(1, 10); // Avance de 1 a 10 metros
-    std::uniform_int_distribution<> tiempo_espera(100, 500); // Espera de 100 a 500 ms
+    std::uniform_int_distribution<> avance_dist(1, 10); 
+    std::uniform_int_distribution<> tiempo_espera(100, 500); 
 
     while (auto_obj.distancia_actual < auto_obj.distancia_total) {
         int avance = avance_dist(gen);
         int espera = tiempo_espera(gen);
 
+        bool ha_llegado = false;
+
         {
-            std::lock_guard<std::mutex> lock(mtx); // Proteger la salida a la consola
+            std::lock_guard<std::mutex> lock(mtx); 
             auto_obj.distancia_actual += avance;
-            if (auto_obj.distancia_actual > auto_obj.distancia_total) {
-                auto_obj.distancia_actual = auto_obj.distancia_total; // No avanzar más de lo permitido
+            if (auto_obj.distancia_actual >= auto_obj.distancia_total) {
+                auto_obj.distancia_actual = auto_obj.distancia_total; 
+                ha_llegado = true;
             }
             std::cout << "Auto" << auto_obj.id << " avanza " << avance << " metros (total: " 
                       << auto_obj.distancia_actual << " metros)" << std::endl;
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(espera)); // Pausa entre iteraciones
-    }
+        if (ha_llegado) {
+            
+            {
+                std::lock_guard<std::mutex> lock(mtx);
+                auto_obj.terminado = true;
+                posicion_llegada++;
+                resultado.push_back({posicion_llegada, auto_obj.id}); 
+                std::cout << "Auto" << auto_obj.id << " ha terminado la carrera en el lugar " 
+                          << posicion_llegada << "!" << std::endl;
+            }
+            break; 
+        }
 
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        auto_obj.terminado = true;
-        posicion_llegada++;
-        resultado.push_back({posicion_llegada, auto_obj.id}); // Guardar la posición de llegada y el id del auto
-        std::cout << "Auto" << auto_obj.id << " ha terminado la carrera en el lugar " 
-                  << posicion_llegada << "!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(espera)); 
     }
 }
 
@@ -60,10 +67,9 @@ int main(int argc, char* argv[]) {
 
     std::vector<Auto> autos;
     std::vector<std::thread> threads;
-    std::vector<std::pair<int, int>> resultado; // Almacenar el lugar de llegada y el id de cada auto
-    int posicion_llegada = 0; // Para registrar la posición de llegada
+    std::vector<std::pair<int, int>> resultado; 
+    int posicion_llegada = 0; 
 
-    // Crear los autos
     for (int i = 0; i < num_autos; ++i) {
         autos.emplace_back(i, distancia_total);
     }
@@ -71,19 +77,16 @@ int main(int argc, char* argv[]) {
     std::cout << "Distancia total carrera: " << distancia_total << " metros" << std::endl;
     std::cout << "-----------------------------" << std::endl;
 
-    // Crear y lanzar las hebras para cada auto
     for (int i = 0; i < num_autos; ++i) {
         threads.emplace_back(correr, std::ref(autos[i]), std::ref(posicion_llegada), std::ref(resultado));
     }
 
-    // Esperar a que todas las hebras terminen
     for (auto &th : threads) {
         if (th.joinable()) {
             th.join();
         }
     }
 
-    // Mostrar el orden de llegada (ya está almacenado en el orden correcto)
     std::cout << "Resultados finales: " << std::endl;
     std::cout << "Lugar  | Auto" << std::endl;
     std::cout << "------------------" << std::endl;
